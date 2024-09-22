9e9a15da8a1945de2a03504341d24755a27e0747
yochainoah <yochyochai123@gmail.com>
2024-09-22 15:35:20 +0300

    fixing bugs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        navigate("/");
+        navigate("/home");
         console.log(user);
       })
       .catch((error) => {
